ed6a824b4efeec61d0e0e2202260c1a95abde773
wanglin-123 <3257632226@qq.com>
2020-03-10 15:51:04 +0800

    3.10作业

--- 基础班作业/201910411122-王淋/201910411122-王淋/1.cpp
+++ 基础班作业/201910411122-王淋/201910411122-王淋/1.cpp
@@ -1,37 +1,12 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
- 
-double fun(int x)
-{
-    double sum = 1;
-    for(int i=1;i<=x;i++){
-        sum *= i;
-    }
-    return sum;
-}
- 
-double cos(int x){
-    double sum = 1;
-    double temp;
-    int i = 2;
-    int a = -1;
-    do{
-        temp=a*pow(x,i)/fun(i);
-        sum += temp;
-        i = i+2;
-        a=-a;
-    }
-	while(abs(temp)>1e-6);
-    return sum;
-}
- 
+#include <iostream>
+using namespace std;
 int main()
 {
-    int x;
-    double sum=0;
-    scanf("%d",&x);
-    sum = cos(x);
-    printf("cos(%d)=%f\n",x,sum);
+    int height;
+    cin>>height;
+    const int a = 12;
+    cout<<"你的身高为: "<<height / 12<<" feet "<<height % 12<<" inches"
+	<<endl;
     return 0;
 }
+
